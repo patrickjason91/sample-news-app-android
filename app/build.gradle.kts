f5f3e93a6054a23be2cf2aadc98b734fb5255bff
Patrick Lim <patrickjasonlim@live.com>
2025-03-28 01:50:10 +0800

    Add and update libraries
    - add jetpack compose libraries
    - add ktx serialization
    - add room and retrofit libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,33 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
+}
+
+val localProps = Properties()
+val localPropertiesFile = project.rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProps.load(FileInputStream(localPropertiesFile))
 }
 
 android {
     namespace = "com.pjlapps.guardiannews"
     compileSdk = 34
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
 
     defaultConfig {
         applicationId = "com.pjlapps.guardiannews"
@@ -15,6 +37,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "GUARDIAN_API_KEY", "\"${localProps.getProperty("theguardian.apiKey")}\"")
+        buildConfigField("String", "GUARDIAN_BASE_URL", "\"https://content.guardianapis.com/\"")
     }
 
     buildTypes {
@@ -42,11 +66,47 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.navigation.compose)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.glide.compose)
+    implementation(libs.gson)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.ktx.serialization.json)
+    implementation(libs.okhttp.logging.interceptor)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+
+    implementation(composeBom)
+    testImplementation(composeBom)
+
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
+
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    testImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.adaptive)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.runtime.livedata)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+kapt {
+    correctErrorTypes = true
 }
